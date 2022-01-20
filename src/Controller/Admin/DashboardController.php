8c36bfec333162f7d144dc51ff2b620b494f52d2
ciboulette-line <77484808+ciboulette-line@users.noreply.github.com>
2022-01-20 14:25:00 +0100

    FLAGME-0006 :sparkles: Session (#12)

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Session;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -25,6 +26,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Sessions', 'fas fa-list', Session::class);
     }
 }
---
+++ src/Controller/Admin/SessionCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Session;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class SessionCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Session::class;
+    }
+
+    public function configureFields(String $pageName): iterable 
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name', 'Name'),
+            BooleanField::new('isClosed', 'Closed'),
+            AssociationField::new('users'),
+        ];
+    }
+}
