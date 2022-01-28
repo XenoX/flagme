7f452ff52bd468e7fef0775285e275658b31acc0
ciboulette-line <77484808+ciboulette-line@users.noreply.github.com>
2022-01-28 09:05:04 +0100

    FLAGME-0017 :sparkles: Formulaire d'envoi de flag (#21)

--- src/Entity/UserFlag.php
+++ src/Entity/UserFlag.php
@@ -6,6 +6,7 @@ use App\Repository\UserFlagRepository;
 use DateTimeImmutable;
 use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserFlagRepository::class)]
 class UserFlag
@@ -17,7 +18,7 @@ class UserFlag
 
     #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'userFlags')]
     #[ORM\JoinColumn(nullable: true)]
-    private ?User $user;
+    private ?UserInterface $user;
 
     #[ORM\ManyToOne(targetEntity: Flag::class, inversedBy: 'userFlags')]
     #[ORM\JoinColumn(nullable: true)]
@@ -36,12 +37,12 @@ class UserFlag
         return $this->id;
     }
 
-    public function getUser(): ?User
+    public function getUser(): ?UserInterface
     {
         return $this->user;
     }
 
-    public function setUser(?User $user): self
+    public function setUser(?UserInterface $user): self
     {
         $this->user = $user;
 
